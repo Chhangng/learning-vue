ab9a3f0948f7e27deb358adc92a34e79ece00ca3
bean chhang <chhang.bean@z1fsc.com>
2023-02-06 15:33:12 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Test from './components/Test.vue'
+import Test1 from './components/Test1.vue'
+
+</script>
+
+<template>
+  <header>
+  
+ 
+  <Test/>
+  <Test1/> 
+
+  <!-- <main>
+  </main> -->
+
+</header>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Test1.vue
@@ -0,0 +1,20 @@
+<script>
+     export default{
+       data(){
+          return {
+            contact :" 12345 Test",
+          }
+       }
+    }
+
+</script>
+
+<template>
+        <h1>test1</h1>
+        <h1>{{contact}}</h1>
+
+</template>
+
+<style>
+
+</style>
